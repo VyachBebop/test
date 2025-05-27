f0cfbfaaa288528661ab0f58b4ab92f31ddfd65d
VyachBebop <vyacheslavkolikov@gmail.com>
2025-05-27 20:41:58 +0300

    Added description

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Я репозиторий");
+        System.out.println("Привет, я репозиторий!");
+        System.out.println("Дата: 27.05.2025");
+
     }
 }
